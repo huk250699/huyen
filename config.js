c9a865ba42b0682423eaf12ab1a931098cf60b83
huk250699 <87457168+huk250699@users.noreply.github.com>
2022-06-19 16:47:51 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Huyền ';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'người yêu tôi';
+const titleCard = 'Tặng em';
+const contentCard = 'lãng mạng ít thôi nha!  chứ làm cho vui thôi nha em';
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
